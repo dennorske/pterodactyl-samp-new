9326821f1714a30a42ff178a6d0c1b6cc44aa642
Simen E <dennorske@users.noreply.github.com>
2020-07-18 22:12:29 +0200

    Fixing some messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 sleep 3
 
 cd /home/container
-
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-
+# export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+echo "Starting $(sampctl -V) on $(uname -a)"
+echo "Will initialize project $(gitproject) by $(gituser)
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
