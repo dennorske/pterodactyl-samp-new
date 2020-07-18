bff4d50e934f3bc7531e8e67be1d4a3e44144093
Simen E <dennorske@users.noreply.github.com>
2020-07-19 00:53:29 +0200

    Fix: Add missing quote

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ sleep 3
 cd /home/container
 # export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 echo "Starting $(sampctl -V) on $(uname -a)"
-echo "Will initialize project $(gitrepo) by $(gituser)
+echo "Will initialize project $(gitrepo) by $(gituser)"
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
